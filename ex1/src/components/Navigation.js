6c103d1e4106547c632daf2e4211f9496faba070
Tomasz Domagalski <tomekwebdev@gmail.com>
2022-05-30 20:21:39 +0200

    fix: LOST APP ;/// - add font awesome and react router

---
+++ ex1/src/components/Navigation.js
@@ -0,0 +1,47 @@
+import React from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBurger, faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { BrowserRouter, Link } from "react-router-dom";
+
+const Logo = <FontAwesomeIcon icon={faBriefcase} />;
+const burgerIcon = <FontAwesomeIcon icon={faBurger} />;
+
+export const Navigation = () => {
+	const [open, setOpen] = useState(false);
+
+	const handleHamburger = () => {
+		setOpen(!open);
+	};
+
+	return (
+		<BrowserRouter>
+			<nav>
+				<div className="navbar-top">
+					<div className="logo">
+						<a href="#">{Logo}</a>
+					</div>
+					<div onClick={handleHamburger} className="ham-menu">
+						<a href="#">{burgerIcon}</a>
+					</div>
+				</div>
+				<div className={open ? "navbar-bottom open" : "navbar-bottom"}>
+					<li>
+						<Link to="/">About me</Link>
+					</li>
+					<li>
+						<Link to="/form">Form</Link>
+					</li>
+					<li>
+						<Link to="/policy">Policy</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
+				</div>
+			</nav>
+		</BrowserRouter>
+	);
+};
+
+export default Navigation;
