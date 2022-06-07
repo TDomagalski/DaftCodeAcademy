3f8e1c01f3d75c1da0178ed4d3fe8443acf6652d
Tomasz Domagalski <tomekwebdev@gmail.com>
2022-06-07 15:42:20 +0200

    docs

--- ex1/src/components/routes/about.js
+++ ex1/src/components/routes/about.js
@@ -4,8 +4,10 @@ export const About = () => {
 			<h1>About me</h1>
 			<h2>Tomasz Domagalski</h2>
 			<h3>
-				Sory za tak późne wysłanie zadania, ale wróciłem niedawno z
-				kawalerskiego -.-
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque
+				similique, commodi numquam consectetur accusamus omnis, quisquam itaque
+				officia delectus ab cumque soluta sequi rem, necessitatibus inventore
+				error hic dignissimos repellat?
 			</h3>
 		</div>
 	);
